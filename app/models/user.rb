49918d6edf833f2d31f3ec1ec9f415fb600e5365
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-01 22:51:56 +0100

    auth configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   has_secure_password
 
   # Model associations
-  # has_many :todos, foreign_key: :created_by
+  has_many :attendances
+
   # Validations
   validates_presence_of :first_name, :last_name, :email, :password_digest
 
   after_create :assign_default_role
-
-  has_many :attendances
 
   def assign_default_role
     self.add_role(:teacher) if self.roles.blank?
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :attendances
+  end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
