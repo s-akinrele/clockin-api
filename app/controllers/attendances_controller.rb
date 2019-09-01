f387b1742c0d88974a2e7c8cfc5dfe6b1b750efa
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-02 00:12:56 +0100

    update and delete endpoint

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,6 @@
 class AttendancesController < ApplicationController
+  before_action :set_attendance, only: [:show, :update, :destroy]
+
   def index
     attendances = current_user.attendances
     json_response(attendances)
@@ -9,9 +11,23 @@ class AttendancesController < ApplicationController
     json_response(attendance, :created)
   end
 
+  def update
+    @attendance.update(attendance_params)
+    head :no_content
+  end
+
+  def destroy
+    @attendance.destroy
+    head :no_content
+  end
+
   private
 
   def attendance_params
-    params.permit(:status)
+    params.permit(:id, :status)
+  end
+
+  def set_attendance
+    @attendance = Attendance.find(params[:id])
   end
 end
