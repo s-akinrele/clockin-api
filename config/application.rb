3b30bfea8efbee16648e2881ca0818663106e4bd
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-03 08:23:05 +0100

    enable cors

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module ClockinApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'https://clockin-client.herokuapp.com/'
+        origins 'http://localhost:3000', 'https://clockin-client.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
